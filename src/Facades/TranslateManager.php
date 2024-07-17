b2971712a02f5f9fb4ba6a85c3eabe0434acc56c
wanghaojie <814425737@qq.com>
2024-07-17 18:19:34 +0800

    feat: Facade提示

--- src/Facades/TranslateManager.php
+++ src/Facades/TranslateManager.php
@@ -2,14 +2,15 @@
 
 namespace Carlin\LaravelTranslateDrivers\Facades;
 
+use Carlin\TranslateDrivers\Providers\AbstractProvider;
 use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static \Carlin\TranslateDrivers\TranslateManager extend(string $driver, \Closure $callback)
- * @method static \Carlin\TranslateDrivers\TranslateManager driver(string $driver = null)
+ * @method static AbstractProvider driver(string $driver = null)
  * @method static \Carlin\TranslateDrivers\TranslateManager config(array $config)
- * @method static \Carlin\TranslateDrivers\TranslateManager getDrivers(): array
- * @method static \Carlin\TranslateDrivers\TranslateManager buildProvider(string $provider, array $config = [])
+ * @method static array getDrivers()
+ * @method static AbstractProvider buildProvider(string $provider, array $config = [])
  *
  * @see \Carlin\TranslateDrivers\TranslateManager
  */
